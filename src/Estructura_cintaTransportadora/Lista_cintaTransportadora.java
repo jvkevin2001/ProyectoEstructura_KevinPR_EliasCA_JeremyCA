f7bf442bd53cf09deffeeb4ebb158d90acaaa692
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-02 00:48:25 -0600

    creacion de metodos en la clase lista_cinstaTransportador

--- src/Estructura_cintaTransportadora/Lista_cintaTransportadora.java
+++ src/Estructura_cintaTransportadora/Lista_cintaTransportadora.java
@@ -1,7 +1,7 @@
 package Estructura_cintaTransportadora;
 
 public class Lista_cintaTransportadora {
-   
+   //atributos//
     private NodoLista_CintaTransportadora cabeza;
     private NodoLista_CintaTransportadora ultimo;
     private int largo;
@@ -10,4 +10,19 @@ public class Lista_cintaTransportadora {
     public  boolean disponible(){
         return disponible;
     }
+    
+    
+    public int tamanio() {
+        return largo;
+    }
+    
+    
+     public boolean vacia(){
+        boolean vacia= false;
+        if(cabeza == null){
+            vacia = true;
+        }
+        return vacia;
+    }
+    
 }
