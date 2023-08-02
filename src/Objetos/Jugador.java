555984a584a2250fc7ed61c19265d406babf2918
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 23:54:00 -0600

    Creacion atributos Jugador

--- src/Objetos/Jugador.java
+++ src/Objetos/Jugador.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Objetos;
 
-/**
- *
- * @author Kevin
- */
 public class Jugador {
     
+    private String id;
+    private int puntos;
+    private int record;
+    private String password;
 }
