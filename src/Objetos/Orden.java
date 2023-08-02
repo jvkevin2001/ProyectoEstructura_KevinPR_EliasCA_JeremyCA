3e378574f790cf669ff4c5bfddc6d18bcd937d65
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 16:17:27 -1200

    Se creo el constructor lleno

--- src/Objetos/Orden.java
+++ src/Objetos/Orden.java
@@ -1,15 +1,27 @@
-
 package Objetos;
 
-
 public class Orden {
+
+    //Creacion de la clase Orden y sus atributos//
+    private int id;
+    private Hamburguesa hamburguesa;
+    private Pila_ingredienteAgregado pila_ingredienteAgregado
+            = new Pila_ingredienteAgregado();
+    private boolean finalizada;
+
+    //Creacion del constructor vacio de la clase orden//
+    public Orden() {
+    }
+
+    //Se creo constructor lleno//
     
-  //Creacion de la clase Orden y sus atributos//
     
-//   private int id;
-//   private Hamburguesa hamburguesa;
-//   private Pila_ingredienteAgregado pila_ingredienteAgregado
-//           = new Pila_ingredienteAgregado();
-//   private boolean finalizada;
-    
+    public Orden(int id, Hamburguesa hamburguesa, boolean finalizada) {
+        this.id = id;
+        this.hamburguesa = hamburguesa;
+        this.finalizada = finalizada;
+    }
+
+   
+
 }
