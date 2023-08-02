bbd89b065839c0ac7b5d7da458d20f61f0660946
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-02 00:45:24 -0600

    creacion de la clase lista_cinstaTransportador

---
+++ src/Estructura_cintaTransportadora/Lista_cintaTransportadora.java
@@ -0,0 +1,13 @@
+package Estructura_cintaTransportadora;
+
+public class Lista_cintaTransportadora {
+   
+    private NodoLista_CintaTransportadora cabeza;
+    private NodoLista_CintaTransportadora ultimo;
+    private int largo;
+    private boolean disponible = true;
+    
+    public  boolean disponible(){
+        return disponible;
+    }
+}
