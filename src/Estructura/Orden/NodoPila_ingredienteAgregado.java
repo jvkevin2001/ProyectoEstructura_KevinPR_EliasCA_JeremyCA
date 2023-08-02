c30ef747edfeb9f1fb2091fc6887e8186fae7bef
jerem <jerem@LAPTOP-E71OG2F9>
2023-08-02 01:48:30 -0600

    creacion de los atributos  en la clase NodoPila_ingredienteAgregado

--- src/Estructura/Orden/NodoPila_ingredienteAgregado.java
+++ src/Estructura/Orden/NodoPila_ingredienteAgregado.java
@@ -1,7 +1,34 @@
 
 package Estructura.Orden;
 
+import Objetos.Ingrediente;
+
 
 public class NodoPila_ingredienteAgregado {
     
+    
+    
+    private Ingrediente ingrediente;
+    private NodoPila_ingredienteAgregado siguiente;
+
+    public NodoPila_ingredienteAgregado() {
+        this.ingrediente = null;
+        this.siguiente = null;
+    }
+
+    public Ingrediente getIngrediente() {
+        return ingrediente;
+    }
+
+    public void setIngrediente(Ingrediente ingrediente) {
+        this.ingrediente = ingrediente;
+    }
+
+    public NodoPila_ingredienteAgregado getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPila_ingredienteAgregado siguiente) {
+        this.siguiente = siguiente;
+    }
 }
--- src/Estructura/Orden/Pila_ingredienteAgregado.java
+++ src/Estructura/Orden/Pila_ingredienteAgregado.java
@@ -1,15 +1,31 @@
-
 package Estructura.Orden;
 
+import Objetos.Ingrediente;
 
 public class Pila_ingredienteAgregado {
-     private NodoPila_ingredienteAgregado cima;
+
+    private NodoPila_ingredienteAgregado cima;
     private int largo;
 
     public boolean Vacia() {
         return cima == null;
     }
+
+    public int tamanio() {
+        return this.largo;
+    }
     
+public void push(Ingrediente ingrediente) {
+        NodoPila_ingredienteAgregado newNodo = new NodoPila_ingredienteAgregado();
+        newNodo.setIngrediente(ingrediente);
+
+        if (Vacia()) {
+            cima = newNodo;
+
+        } else {
+            newNodo.setSiguiente(cima);
+            cima = newNodo;
+        }
+        largo++;
+    }
 }
-
-
