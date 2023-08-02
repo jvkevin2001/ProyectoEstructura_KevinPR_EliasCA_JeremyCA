f51b0440b220ca4088110760db855fea32dcf443
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 23:27:33 -0600

    creacion Clase y Atributos Pila Ingrediente

---
+++ src/Estructura_Pila_Ingredientes/Pila_Ingrediente.java
@@ -0,0 +1,10 @@
+package Estructura_Pila_Ingredientes;
+
+import Objetos.Ingrediente;
+
+public class Pila_Ingrediente {
+    //Atributos
+   private Ingrediente ingrediente;
+   private Nodo_Pila_Ingrediente siguiente; 
+   
+}
