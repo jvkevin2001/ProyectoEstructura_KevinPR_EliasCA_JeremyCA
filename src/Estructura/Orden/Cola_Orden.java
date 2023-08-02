5a90760aef4a63f0f83025f0dce63823e6c5797b
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 15:45:04 -1200

    Se creo el metodo vacio que me indica si la cola esta vacia o no

--- src/Estructura/Orden/Cola_Orden.java
+++ src/Estructura/Orden/Cola_Orden.java
@@ -1,16 +1,35 @@
-
 package Estructura.Orden;
 
-
 public class Cola_Orden {
-   
-      //
+
+    //Se creo la clase cola_orden//
     private NodoCola_Orden frente;
     private NodoCola_Orden ultimo;
     private int largo;
 
     public int tamanio() {
         return largo;
+
     }
-    
+
+    //Se creo el metodo que me muestra si la orden esta vacia o no//
+    public boolean Vacia() {
+        return frente == null;
+    }
+
+//    public void encola(Orden orden) {
+//
+//        NodoCola_Orden newNodo = new NodoCola_Orden();
+//        newNodo.setOrden(orden);
+//
+//        if (Vacia()) {
+//            frente = newNodo;
+//            ultimo = newNodo;
+//        } else {
+//            ultimo.setAtras(newNodo);
+//            ultimo = newNodo;
+//        }
+//        largo++;
+//
+//    }
 }
