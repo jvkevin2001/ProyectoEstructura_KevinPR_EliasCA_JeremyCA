ed23e386bb4475a41b48f7d1fa0c25fc3df1fe55
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 22:53:38 -0600

    Creacion Constructor Hamburguesa

--- src/Objetos/Hamburguesa.java
+++ src/Objetos/Hamburguesa.java
@@ -3,9 +3,15 @@ package Objetos;
 
 
 public class Hamburguesa {
+    //Atributos
     private String tipo;
     private Pila_Ingrediente pila_ingredientes = new Pila_Ingrediente();
     private int puntos;
     
+    //Constructor
+    public Hamburguesa(String tipo, int puntos) {
+        this.tipo = tipo;
+        this.puntos = puntos;
+    }
     
 }
