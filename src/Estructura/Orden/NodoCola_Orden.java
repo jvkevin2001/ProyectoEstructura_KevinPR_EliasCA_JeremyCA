d619a51a471ff701ac11e794a35291b35b0779c5
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 16:02:01 -1200

    Creacion del constructor lleno de la clase NodoCola

--- src/Estructura/Orden/NodoCola_Orden.java
+++ src/Estructura/Orden/NodoCola_Orden.java
@@ -6,8 +6,16 @@ public class NodoCola_Orden {
    
    //Creacion de los atributos de la clase NodoCola//
     
-   private Orden orden; 
-   private NodoCola_Orden atras;
+   
+    private Orden orden; 
+    private NodoCola_Orden atras;
+
+   //Creacion del constructor lleno de la clase NodoCola//
+    
+    public NodoCola_Orden(Orden orden, NodoCola_Orden atras) {
+        this.orden = orden;
+        this.atras = atras;
+    }
 
     
 }
