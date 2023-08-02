50ce3b052a4f67679ba87f9d6cce087855dedfe2
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 23:16:09 -0600

    creacion atributos Nodo Pila Ingrediente

--- src/Estructura_Pila_Ingredientes/Nodo_Pila_Ingrediente.java
+++ src/Estructura_Pila_Ingredientes/Nodo_Pila_Ingrediente.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Estructura_Pila_Ingredientes;
 
-/**
- *
- * @author Kevin
- */
+import Objetos.Ingrediente;
+
 public class Nodo_Pila_Ingrediente {
     
+//Atributos
+private Ingrediente ingrediente;
+private Nodo_Pila_Ingrediente siguiente;
 }
