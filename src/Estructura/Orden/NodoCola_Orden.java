272939c57656319e096d0b54ff792e107486076e
Elías <Elas@DESKTOP-2KETQI6>
2023-08-01 16:11:21 -1200

    Creacion de la clase orden y los atributos.

--- src/Estructura/Orden/NodoCola_Orden.java
+++ src/Estructura/Orden/NodoCola_Orden.java
@@ -7,33 +7,33 @@ public class NodoCola_Orden {
    //Creacion de los atributos de la clase NodoCola//
     
    
-    private Orden orden; 
-    private NodoCola_Orden atras;
-
-   //Creacion del constructor lleno de la clase NodoCola//
-    
-    public NodoCola_Orden(Orden orden, NodoCola_Orden atras) {
-        this.orden = orden;
-        this.atras = atras;
-    }
-
-    //Se crearon los getts and setters de la clase NodoCola
-    
-    public Orden getOrden() {
-        return orden;
-    }
-
-    public void setOrden(Orden orden) {
-        this.orden = orden;
-    }
-
-    public NodoCola_Orden getAtras() {
-        return atras;
-    }
-
-    public void setAtras(NodoCola_Orden atras) {
-        this.atras = atras;
-    }
+//    private Orden orden; 
+//    private NodoCola_Orden atras;
+//
+//   //Creacion del constructor lleno de la clase NodoCola//
+//    
+//    public NodoCola_Orden(Orden orden, NodoCola_Orden atras) {
+//        this.orden = orden;
+//        this.atras = atras;
+//    }
+//
+//    //Se crearon los getts and setters de la clase NodoCola
+//    
+//    public Orden getOrden() {
+//        return orden;
+//    }
+//
+//    public void setOrden(Orden orden) {
+//        this.orden = orden;
+//    }
+//
+//    public NodoCola_Orden getAtras() {
+//        return atras;
+//    }
+//
+//    public void setAtras(NodoCola_Orden atras) {
+//        this.atras = atras;
+//    }
 
     
 }
---
+++ src/Objetos/Orden.java
@@ -0,0 +1,15 @@
+
+package Objetos;
+
+
+public class Orden {
+    
+  //Creacion de la clase Orden y sus atributos//
+    
+//   private int id;
+//   private Hamburguesa hamburguesa;
+//   private Pila_ingredienteAgregado pila_ingredienteAgregado
+//           = new Pila_ingredienteAgregado();
+//   private boolean finalizada;
+    
+}
