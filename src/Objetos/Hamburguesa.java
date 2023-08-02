ade5ae8035239e26e75e57f18dadd557d5a8cc5a
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 22:52:44 -0600

    Creacion atributos Hamburguesa

--- src/Objetos/Hamburguesa.java
+++ src/Objetos/Hamburguesa.java
@@ -3,5 +3,9 @@ package Objetos;
 
 
 public class Hamburguesa {
+    private String tipo;
+    private Pila_Ingrediente pila_ingredientes = new Pila_Ingrediente();
+    private int puntos;
+    
     
 }
