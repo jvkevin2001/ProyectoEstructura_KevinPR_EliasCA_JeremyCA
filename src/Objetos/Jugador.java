e2318746143cbf3210bc8cbf0a96e8c75ea672ce
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-01 23:55:46 -0600

    Creacion Constructor Jugador

--- src/Objetos/Jugador.java
+++ src/Objetos/Jugador.java
@@ -3,8 +3,22 @@ package Objetos;
 
 public class Jugador {
     
+    //Atributos
     private String id;
     private int puntos;
     private int record;
     private String password;
+    
+    //Constructores
+    public Jugador() {
+    }
+
+    public Jugador(String id, int puntos, int record, String password) {
+        this.id = id;
+        this.puntos = puntos;
+        this.record = record;
+        this.password = password;
+    }
+    
+    
 }
