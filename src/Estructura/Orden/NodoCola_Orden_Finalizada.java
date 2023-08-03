2bdd590686663d8254995fb03ba1d4cd4cfb9eef
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-02 21:17:46 -0600

    Creacion de Setter and getter NodoCola_Orden_Finalizada

--- src/Estructura/Orden/NodoCola_Orden_Finalizada.java
+++ src/Estructura/Orden/NodoCola_Orden_Finalizada.java
@@ -3,13 +3,34 @@ package Estructura.Orden;
 import Objetos.Orden;
 
 public class NodoCola_Orden_Finalizada {
+   //Atributos
    private Orden orden; 
    private NodoCola_Orden_Finalizada atras;
-
+   
+   //Metodo Constructor
     public NodoCola_Orden_Finalizada() {
         this.orden = null;
         this.atras = null;
     }
+    
+    //Setter
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+    public void setAtras(NodoCola_Orden_Finalizada atras) {
+        this.atras = atras;
+    }
+    
+    //Getter
+    public Orden getOrden() {
+        return orden;
+    }
+    public NodoCola_Orden_Finalizada getAtras() {
+        return atras;
+    }
+    
+    
+    
    
    
 }
