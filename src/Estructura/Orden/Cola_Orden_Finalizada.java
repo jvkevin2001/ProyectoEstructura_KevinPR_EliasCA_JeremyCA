0508e25f305f52770dfd5b22c376435eac959c54
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-02 21:25:42 -0600

    Creacion de Atributos Cola_Orden_Finalizada

--- src/Estructura/Orden/Cola_Orden_Finalizada.java
+++ src/Estructura/Orden/Cola_Orden_Finalizada.java
@@ -1,7 +1,11 @@
 package Estructura.Orden;
 
-
+import Objetos.Orden;
 
 public class Cola_Orden_Finalizada {
     
+    //Atributos
+    private NodoCola_Orden_Finalizada frente;
+    private NodoCola_Orden_Finalizada ultimo;
+    private int largo;
 }
