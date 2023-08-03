d31d05f0b03b281b4553a79d3136924574771c22
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-02 21:12:24 -0600

    Creacion de MetodoConstructor NodoCola_Orden_Finalizada

--- src/Estructura/Orden/NodoCola_Orden_Finalizada.java
+++ src/Estructura/Orden/NodoCola_Orden_Finalizada.java
@@ -5,4 +5,11 @@ import Objetos.Orden;
 public class NodoCola_Orden_Finalizada {
    private Orden orden; 
    private NodoCola_Orden_Finalizada atras;
+
+    public NodoCola_Orden_Finalizada() {
+        this.orden = null;
+        this.atras = null;
+    }
+   
+   
 }
