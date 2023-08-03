a17d2431bb4c5a63ac7034a207b24b484e8ebf7d
jvkevin2001 <ramirezkevin6165@gmail.com>
2023-08-02 21:27:58 -0600

    Creacion de Metodo tamanio Cola_Orden_Finalizada

--- src/Estructura/Orden/Cola_Orden_Finalizada.java
+++ src/Estructura/Orden/Cola_Orden_Finalizada.java
@@ -8,4 +8,10 @@ public class Cola_Orden_Finalizada {
     private NodoCola_Orden_Finalizada frente;
     private NodoCola_Orden_Finalizada ultimo;
     private int largo;
+    
+    //Metodo tamanio
+    public int tamanio(){
+        return largo;
+    }
+    
 }
